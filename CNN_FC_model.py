777331754d8df77116ebeff2c2dc7bf0043939bf
zengbohan0217 <13811753844@126.com>
2021-01-12 22:24:08 +0800

    add the self attention

--- CNN_FC_model.py
+++ CNN_FC_model.py
@@ -13,20 +13,52 @@ import numpy as np
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # DEVICE = torch.device("cpu")
 
-class attention_FC(nn.Module):
-    def __init__(self, channel=224):
-        self.FC1 = nn.Sequential(nn.Linear(24020, 10000), nn.Dropout(p=0.5))
-        self.FC2 = nn.Sequential(nn.Linear(10000, 224), nn.Dropout(p=0.5))
+class attention_FC224(nn.Module):
+    def __init__(self, channel=224, down_scale=3):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1 = nn.Conv2d(channel, channel // 16, 1, padding=1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(channel // 16, channel // down_scale, 1, padding=0, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
     def forward(self, x):
         """
         :param x: a tensor which size is n*224*3*3
         :return: x with self-attention
         """
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return out
 
+class attention_FC102(nn.Module):
+    def __init__(self, channel=102, down_scale=3):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1 = nn.Conv2d(channel, channel // 16, 1, padding=1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(channel // 16, channel // down_scale, 1, padding=0, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        """
+        :param x: a tensor which size is n*224*3*3
+        :return: x with self-attention
+        """
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return out
 
 class CNN3Net_224(nn.Module):
     def __init__(self, active_fc='PReLU', pretrained=False, model_pth=None):
         super().__init__()
+        self.attention_conv = attention_FC224()
         self.conv3D = nn.Conv3d(1, 20, kernel_size=(24, 3, 3), padding=(0, 1, 1))
         if pretrained and model_pth != None:
             model_pre = torch.load(model_pth, map_location='cuda')
@@ -39,14 +71,15 @@ class CNN3Net_224(nn.Module):
             self.Act_F = nn.ELU()
         elif active_fc == 'SELU':
             self.Act_F = nn.SELU()
-        self.FC1 = nn.Sequential(nn.Linear(24020, 10000), nn.Dropout(p=0.5))
-        self.FC2 = nn.Sequential(nn.Linear(10000, 15), nn.Dropout(p=0.5))
+        self.FC1 = nn.Sequential(nn.Linear(6020, 1000), nn.Dropout(p=0.5))
+        self.FC2 = nn.Sequential(nn.Linear(1000, 15), nn.Dropout(p=0.5))
 
     def forward(self, x):
         # input shape is batch_size * 224 * 3 * 3
         b_size = x.size(0)
+        x = self.attention_conv(x)
         x = F.interpolate(x, scale_factor=2, mode='nearest') # reshape 3*3 into 6*6
-        x = x.view(-1, 1, 224, 6, 6)
+        x = x.view(-1, 1, 74, 6, 6)
         conv_res = self.conv3D(x)
         # print(conv_res.size())
         pool1 = F.max_pool3d(conv_res, 6)
@@ -66,6 +99,7 @@ class CNN3Net_224(nn.Module):
 class CNN3Net_102(nn.Module):
     def __init__(self, active_fc='PReLU', pretrained=False, model_pth=None):
         super().__init__()
+        self.attention_conv = attention_FC102()
         self.conv3D = nn.Conv3d(1, 20, kernel_size=(24, 3, 3), padding=(0, 1, 1))
         if pretrained and model_pth != None:
             model_pre = torch.load(model_pth, map_location='cuda')
@@ -78,14 +112,15 @@ class CNN3Net_102(nn.Module):
             self.Act_F = nn.ELU()
         elif active_fc == 'SELU':
             self.Act_F = nn.SELU()
-        self.FC1 = nn.Sequential(nn.Linear(9360, 10000), nn.Dropout(p=0.5))
-        self.FC2 = nn.Sequential(nn.Linear(10000, 15), nn.Dropout(p=0.5))
+        self.FC1 = nn.Sequential(nn.Linear(1160, 1000), nn.Dropout(p=0.5))
+        self.FC2 = nn.Sequential(nn.Linear(1000, 15), nn.Dropout(p=0.5))
 
     def forward(self, x):
         # input shape is batch_size * 102 * 3 * 3
         b_size = x.size(0)
+        x = self.attention_conv(x)
         x = F.interpolate(x, scale_factor=2, mode='nearest') # reshape 3*3 into 6*6
-        x = x.view(-1, 1, 102, 6, 6)
+        x = x.view(-1, 1, 34, 6, 6)
         conv_res = self.conv3D(x)
         pool1 = F.max_pool3d(conv_res, 6)
         pool2 = F.max_pool3d(conv_res, 3)
